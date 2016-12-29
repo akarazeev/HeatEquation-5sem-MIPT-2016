71cd8a9365b108781b2301c19e03e2aed884f300
Anton Karazeev <AntonKarazeev@Antons-MacBook-Pro.local>
2016-12-29 18:24:29 +0300

    Made some cleaning

--- work_downloads.sh
+++
@@ -1,5 +0,0 @@
-rm -rf images/* &&
-./tools/draw_plots_downloads.py &&
-cd images &&
-ffmpeg -f image2 -pattern_type glob -framerate 12 -i '*.png' -s 400x400 foo.mp4 &&
-ffmpeg -i foo.mp4 -pix_fmt rgb24 -s qcif -loop 0 -s 400x400 output.gif
